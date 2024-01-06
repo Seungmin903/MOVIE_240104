f12603976e10a09190a87edd18538b6b5ebd81a7
Seungmin <seungmin903@naver.com>
2024-01-07 05:03:33 +0900

    상세페이지 만들기 전

--- src/App.js
+++ src/App.js
@@ -1,53 +1,20 @@
 import React from "react";
-import axios from "axios";
-import Movie from "./Movie";
 import './App.css';
+import { HashRouter, Route, Routes } from "react-router-dom";
+import About from './routes/About';
+import Home from "./routes/Home";
+import Navigation from "./components/Navigation";
 
-class App extends React.Component{
-  state = {
-    isLoading : true,
-    movies: [],
-  };
-
-  getMovies = async () => {//async = getmovies 함수에게 시간이 필요 await = axios.get 시간을 기다려 달라
-    const {
-      data: {
-        data : {movies},
-      },
-    } =  await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
-    
-    this.setState({movies, isLoading : false});
-  };
-
-  componentDidMount(){
-    this.getMovies();
-  }
-
-  render(){
-    const {isLoading,movies} = this.state;
-    return(
-    <section className = "container">
-      {isLoading ?(
-        <div className = "loader">
-          <span className = "loader__text">Loading...</span>
-          </div>
-      ) : (
-        <div className ="movies">
-        {movies.map((movie) => (
-        <Movie 
-          key ={movie.id}
-          id = {movie.id}
-          year = {movie.year}
-          title = {movie.title}
-          summary = {movie.summary}
-          poster = {movie.poster}
-          genres = {movie.genres}
-        />
-        ))}
-      </div>
-    )}
-    </section>
-    );
-  }
+function App(){
+  return(
+    <HashRouter>
+      <Navigation />
+      <Routes>
+        <Route path = "/" exact = {true} Component = {Home}/>
+        <Route path = "/about" Component = {About}/>
+      </Routes>
+    </HashRouter>
+  );
 }
+
 export default App;
---
+++ src/routes/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+function About(props){
+    console.log(props);
+    return (
+        <div className = "about__container">
+
+            <span>
+                "About this page : I'm cloning"
+            </span>
+        </div>
+    );
+}
+export default About;
